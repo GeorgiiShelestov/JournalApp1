0a5933cc6fa9f951e1fed39b976165ebec0b4004
Mf <Funfayk082@gmail.com>
2023-03-28 11:29:37 +0300

    Добавьте файлы проекта.

---
+++ JournalApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using JournalApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalApp.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title;
+
+        //<summary>Заголовок окна</summary>
+
+        public string Title
+        {
+            get => _Title;
+            ////set
+            ////{
+            ////    //if (Equals(_Title, value)) return;
+            ////    //_Title = value;
+            ////    //OnPropertyChanged();
+
+            ////    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        } 
+        #endregion
+    }
+}
